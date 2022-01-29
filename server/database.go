bdafd8ca2dcbafecdaf0663f5a506be8f1917bb3
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-01-29 16:30:31 -0500

    Adding db hardcoded fields

--- server/database.go
+++ server/database.go
@@ -22,19 +22,21 @@ func setDatabase() {
 	db.AutoMigrate(&University{})
 
 	// Create
-	db.Create(&University{Name: "University of Florida", Ranking: 5, Country: "USA"})
+	db.Create(&University{Name: "University of Florida", Ranking: 1, Country: "USA"})
+	db.Create(&University{Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"})
+	db.Create(&University{Name: "Stony Brook", Ranking: 3, Country: "USA"})
 
-	// Read
-	var university University
-	db.First(&university, 1)                  // find product with integer primary key
-	db.First(&university, "ranking = ?", "5") // find product with code D42
+	// // Read
+	// var university University
+	// db.First(&university, 1)                  // find product with integer primary key
+	// db.First(&university, "ranking = ?", "5") // find product with code D42
 
-	// Update - update product's price to 200
-	db.Model(&university).Update("Ranking", 1)
-	// Update - update multiple fields
-	db.Model(&university).Updates(University{Ranking: 6, Country: "United States of America"}) // non- fields
-	db.Model(&university).Updates(map[string]interface{}{"Ranking": 6, "Country": "United States of America"})
+	// // Update - update product's price to 200
+	// db.Model(&university).Update("Ranking", 1)
+	// // Update - update multiple fields
+	// db.Model(&university).Updates(University{Ranking: 6, Country: "United States of America"}) // non- fields
+	// db.Model(&university).Updates(map[string]interface{}{"Ranking": 6, "Country": "United States of America"})
 
-	// Delete - delete product
-	//db.Delete(&university, 1)
+	// // Delete - delete product
+	// db.Delete(&university, 1)
 }
