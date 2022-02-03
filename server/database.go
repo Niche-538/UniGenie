cb39eb46b34b7d455932bcc46933fb08b3f29831
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-02-02 19:20:26 -0500

    Refractor

--- server/database.go
+++ server/database.go
@@ -7,16 +7,18 @@ import (
 
 type University struct {
 	gorm.Model
-	Name    string
-	Ranking uint
-	Country string
+	ID      string `gorm:"primary_key" json:"id"`
+	Name    string `json:"name"`
+	Ranking uint   `json:"ranking"`
+	Country string `json:"country"`
 }
 
 type User struct {
 	gorm.Model
-	Name     string
-	Username string
-	Password string
+	ID       string `gorm:"primary_key" json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func setDatabase() {
@@ -29,21 +31,21 @@ func setDatabase() {
 	db.AutoMigrate(&University{})
 	db.AutoMigrate(&User{})
 
-	// // Create
-	// db.Create(&University{Name: "University of Florida", Ranking: 1, Country: "USA"})
-	// db.Create(&University{Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"})
-	// db.Create(&University{Name: "Stony Brook", Ranking: 3, Country: "USA"})
+	// Create
+	db.Create(&University{Name: "University of Florida", Ranking: 1, Country: "USA"})
+	db.Create(&University{Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"})
+	db.Create(&University{Name: "Stony Brook", Ranking: 3, Country: "USA"})
 
-	// // Read
-	// var university University
-	// db.First(&university, 1)                  // find product with integer primary key
-	// db.First(&university, "ranking = ?", "5") // find product with code D42
+	// Read
+	var university University
+	db.First(&university, 1)                  // find product with integer primary key
+	db.First(&university, "ranking = ?", "5") // find product with code D42
 
-	// // Update - update product's price to 200
-	// db.Model(&university).Update("Ranking", 1)
-	// // Update - update multiple fields
-	// db.Model(&university).Updates(University{Ranking: 6, Country: "United States of America"}) // non- fields
-	// db.Model(&university).Updates(map[string]interface{}{"Ranking": 6, "Country": "United States of America"})
+	// Update - update product's price to 200
+	db.Model(&university).Update("Ranking", 1)
+	// Update - update multiple fields
+	db.Model(&university).Updates(University{Ranking: 6, Country: "United States of America"}) // non- fields
+	db.Model(&university).Updates(map[string]interface{}{"Ranking": 6, "Country": "United States of America"})
 
 	// // Delete - delete product
 	// db.Delete(&university, 1)
