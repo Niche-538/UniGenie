c2508c53861c69adc736dad52462db51578da971
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-02-03 08:04:05 -0500

    Minor Changes

--- server/database.go
+++ server/database.go
@@ -7,12 +7,14 @@ import (
 
 type University struct {
 	gorm.Model
-	UniversityID uint   `gorm:"primaryKey;autoIncrement" json:"university ID"`
-	Name         string `json:"name"`
-	Ranking      uint   `json:"ranking"`
-	Country      string `json:"country"`
+	ID      uint   `gorm:"primaryKey;autoIncrement" json:"university ID"`
+	Name    string `json:"name"`
+	Ranking uint   `json:"ranking"`
+	Country string `json:"country"`
 	// CreatedAt    time.Time `json:"createdAt"`
 	// UpdatedAt    time.Time `json:"updatedAt"`
+	// UniversityID uint   `gorm:"primaryKey;autoIncrement" json:"university ID"`
+
 }
 
 // var universities = []University{
@@ -23,16 +25,17 @@ type University struct {
 
 type User struct {
 	gorm.Model
-	UserID   uint   `gorm:"primaryKey;autoIncrement" json:"User ID"`
+	ID       uint   `gorm:"primaryKey;autoIncrement" json:"User ID"`
 	Name     string `json:"name"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	// UserID   uint   `gorm:"primaryKey;autoIncrement" json:"User ID"`
 }
 
-type CoursesOffered struct {
+type OfferedCourse struct {
 	gorm.Model
-	CourseID     uint `gorm:"primaryKey;autoIncrement" json:"Course ID"`
-	UniversityID uint `json:"University ID Foreign Key"`
+	ID uint `gorm:"primaryKey;autoIncrement" json:"Course ID"`
+	// UniversityID uint `json:"University ID Foreign Key"`
 }
 
 func setDatabase() {
@@ -44,15 +47,26 @@ func setDatabase() {
 	// Migrate the schema
 	db.AutoMigrate(&University{})
 	db.AutoMigrate(&User{})
-	db.AutoMigrate(&CoursesOffered{})
+	db.AutoMigrate(&OfferedCourse{})
 
-	var unis = []University{{UniversityID: 1, Name: "University of Florida", Ranking: 1, Country: "USA"},
-		{UniversityID: 2, Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"},
-		{UniversityID: 3, Name: "Stony Brook", Ranking: 3, Country: "USA"}}
+	var unis = []University{{Name: "University of Florida", Ranking: 1, Country: "USA"},
+		{Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"},
+		{Name: "Stony Brook", Ranking: 3, Country: "USA"}}
 
 	db.Create(&unis)
 	// db.Migrator().DropTable(&User{})
 	// db.Migrator().DropTable(&University{})
 	// db.Migrator().DropTable(&CoursesOffered{})
 
+	// db.Migrator().DropConstraint(&User{}, "primary_key")
+	// db.Migrator().DropConstraint(&University{}, "primary_key")
+	// db.Migrator().DropConstraint(&CoursesOffered{}, "primary_key")
+
+	// db.Migrator().DropColumn(&User{}, "id")
+	// db.Migrator().DropColumn(&University{}, "id")
+	// db.Migrator().DropColumn(&CoursesOffered{}, "id")
+
 }
+
+// func returnUniversities() {
+// }
