cfac04755bd486fdc355a538661ed80dceb6e559
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-02-03 05:16:26 -0500

    Temporary Changes

--- server/database.go
+++ server/database.go
@@ -7,31 +7,33 @@ import (
 
 type University struct {
 	gorm.Model
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	Ranking uint   `json:"ranking"`
-	Country string `json:"country"`
+	UniversityID uint   `gorm:"primaryKey;autoIncrement" json:"university ID"`
+	Name         string `json:"name"`
+	Ranking      uint   `json:"ranking"`
+	Country      string `json:"country"`
+	// CreatedAt    time.Time `json:"createdAt"`
+	// UpdatedAt    time.Time `json:"updatedAt"`
 }
 
-var universities = []University{
-	{ID: "1", Name: "University of Florida", Ranking: 1, Country: "USA"},
-	{ID: "2", Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"},
-	{ID: "3", Name: "Stony Brook", Ranking: 3, Country: "USA"},
-}
+// var universities = []University{
+// 	{UniversityID: 1, Name: "University of Florida", Ranking: 1, Country: "USA"},
+// 	{UniversityID: 2, Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"},
+// 	{UniversityID: 3, Name: "Stony Brook", Ranking: 3, Country: "USA"},
+// }
 
 type User struct {
 	gorm.Model
-	ID       string `json:"id"`
+	UserID   uint   `gorm:"primaryKey;autoIncrement" json:"User ID"`
 	Name     string `json:"name"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-// type CoursesOffered struct {
-// 	gorm.Model
-// 	CourseID     uint `gorm:"primary_key" json:"id"`
-// 	universityID uint
-// }
+type CoursesOffered struct {
+	gorm.Model
+	CourseID     uint `gorm:"primaryKey;autoIncrement" json:"Course ID"`
+	UniversityID uint `json:"University ID Foreign Key"`
+}
 
 func setDatabase() {
 	db, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
@@ -42,4 +44,15 @@ func setDatabase() {
 	// Migrate the schema
 	db.AutoMigrate(&University{})
 	db.AutoMigrate(&User{})
+	db.AutoMigrate(&CoursesOffered{})
+
+	var unis = []University{{UniversityID: 1, Name: "University of Florida", Ranking: 1, Country: "USA"},
+		{UniversityID: 2, Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"},
+		{UniversityID: 3, Name: "Stony Brook", Ranking: 3, Country: "USA"}}
+
+	db.Create(&unis)
+	// db.Migrator().DropTable(&User{})
+	// db.Migrator().DropTable(&University{})
+	// db.Migrator().DropTable(&CoursesOffered{})
+
 }
--- server/main.go
+++ server/main.go
@@ -6,9 +6,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func getUniversities(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, universities)
-}
+// func getUniversities(c *gin.Context) {
+// 	c.IndentedJSON(http.StatusOK, universities)
+// }
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
@@ -25,7 +25,7 @@ func setupRouter() *gin.Engine {
 func main() {
 	r := setupRouter()
 	setDatabase()
-	r.GET("/universities", getUniversities)
+	// r.GET("/universities", getUniversities)
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
