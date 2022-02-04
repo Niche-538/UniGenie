12933e374333634b0a0168c3d757043a99462a3b
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-02-04 18:06:14 -0500

    Adding Get API

--- server/database.go
+++ server/database.go
@@ -1,72 +1,24 @@
 package main
 
-import (
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
+// import (
+// 	"gorm.io/driver/sqlite"
+// 	"gorm.io/gorm"
+// )
 
-type University struct {
-	gorm.Model
-	ID      uint   `gorm:"primaryKey;autoIncrement" json:"university ID"`
-	Name    string `json:"name"`
-	Ranking uint   `json:"ranking"`
-	Country string `json:"country"`
-	// CreatedAt    time.Time `json:"createdAt"`
-	// UpdatedAt    time.Time `json:"updatedAt"`
-	// UniversityID uint   `gorm:"primaryKey;autoIncrement" json:"university ID"`
+// func setDatabase() {
+// 	db, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
+// 	if err != nil {
+// 		panic("failed to connect database")
+// 	}
 
-}
+// 	// Migrate the schema
+// 	db.AutoMigrate(&University{})
+// 	db.AutoMigrate(&User{})
+// 	db.AutoMigrate(&OfferedCourse{})
 
-// var universities = []University{
-// 	{UniversityID: 1, Name: "University of Florida", Ranking: 1, Country: "USA"},
-// 	{UniversityID: 2, Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"},
-// 	{UniversityID: 3, Name: "Stony Brook", Ranking: 3, Country: "USA"},
-// }
+// 	var unis = []University{{Name: "University of Florida", Ranking: 1, Country: "USA"},
+// 		{Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"},
+// 		{Name: "Stony Brook", Ranking: 3, Country: "USA"}}
 
-type User struct {
-	gorm.Model
-	ID       uint   `gorm:"primaryKey;autoIncrement" json:"User ID"`
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	// UserID   uint   `gorm:"primaryKey;autoIncrement" json:"User ID"`
-}
-
-type OfferedCourse struct {
-	gorm.Model
-	ID uint `gorm:"primaryKey;autoIncrement" json:"Course ID"`
-	// UniversityID uint `json:"University ID Foreign Key"`
-}
-
-func setDatabase() {
-	db, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	// Migrate the schema
-	db.AutoMigrate(&University{})
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&OfferedCourse{})
-
-	var unis = []University{{Name: "University of Florida", Ranking: 1, Country: "USA"},
-		{Name: "University of Texas, Dallas", Ranking: 2, Country: "USA"},
-		{Name: "Stony Brook", Ranking: 3, Country: "USA"}}
-
-	db.Create(&unis)
-	// db.Migrator().DropTable(&User{})
-	// db.Migrator().DropTable(&University{})
-	// db.Migrator().DropTable(&CoursesOffered{})
-
-	// db.Migrator().DropConstraint(&User{}, "primary_key")
-	// db.Migrator().DropConstraint(&University{}, "primary_key")
-	// db.Migrator().DropConstraint(&CoursesOffered{}, "primary_key")
-
-	// db.Migrator().DropColumn(&User{}, "id")
-	// db.Migrator().DropColumn(&University{}, "id")
-	// db.Migrator().DropColumn(&CoursesOffered{}, "id")
-
-}
-
-// func returnUniversities() {
+// 	db.Create(&unis)
 // }
--- server/main.go
+++ server/main.go
@@ -4,19 +4,44 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-// func getUniversities(c *gin.Context) {
-// 	db, _ := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
-// 	// var universities []map[string]interface{}
-// 	// db.Table("University", " ").Find(&universities)
+type University struct {
+	gorm.Model
+	ID      uint   `gorm:"primaryKey;autoIncrement" json:"university ID"`
+	Name    string `json:"name"`
+	Ranking uint   `json:"ranking"`
+	Country string `json:"country"`
+}
 
-// 	universities := db.Table("University").Select("name")
-// 	fmt.Println(universities)
+type User struct {
+	gorm.Model
+	ID       uint   `gorm:"primaryKey;autoIncrement" json:"User ID"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
-// 	// db.Find(&University)
-// 	c.IndentedJSON(http.StatusOK, universities)
-// }
+type OfferedCourse struct {
+	gorm.Model
+	ID         uint   `gorm:"primaryKey;autoIncrement" json:"Course ID"`
+	CourseName string `json:"course_name"`
+}
+
+func getUniversities(c *gin.Context) {
+	db, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	universities := []University{}
+
+	db.Find(&universities)
+	c.JSON(200, &universities)
+
+}
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
@@ -32,36 +57,9 @@ func setupRouter() *gin.Engine {
 
 func main() {
 	r := setupRouter()
-	setDatabase()
 
-	// type University struct {
-	// 	gorm.Model
-	// 	UniversityID uint   `gorm:"primaryKey;autoIncrement" json:"university ID"`
-	// 	Name         string `json:"name"`
-	// 	Ranking      uint   `json:"ranking"`
-	// 	Country      string `json:"country"`
-	// 	// CreatedAt    time.Time `json:"createdAt"`
-	// 	// UpdatedAt    time.Time `json:"updatedAt"`
-	// }
+	r.GET("/universities", getUniversities)
 
-	// db, _ := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
-	// var universities []map[string]interface{}
-	// db.Table("University", " ").Find(&universities)
-
-	// var univ University
-	// res := map[string]interface{}{}
-
-	// fmt.Println("")
-	// fmt.Println("###########################                   #####################################")
-	// rows, _ := db.Table("Universities").Select("name").Rows()
-	// db.Table("Universities").Take(&res)
-	// for rows.Next() {
-	// 	x := rows.Scan()
-	// 	fmt.Println(x)
-	// }
-	// fmt.Println("###########################                   #####################################")
-	// fmt.Println("")
-	// r.GET("/universities", getUniversities)
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
