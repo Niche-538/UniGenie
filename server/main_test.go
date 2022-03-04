a9e9ecf60e68fd38309dc79d4df092bd3f57d9af
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-03-03 19:00:48 -0500

    Adding Test Case for GetUsers

--- server/main_test.go
+++ server/main_test.go
@@ -32,3 +32,27 @@ func TestGetUniversities(t *testing.T) {
 		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
 	}
 }
+
+func TestGetUsers(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	r := gin.Default()
+	r.GET("/getUsers", getUsers)
+
+	req, err := http.NewRequest(http.MethodGet, "/getUsers", nil)
+
+	if err != nil {
+		t.Fatalf("Couldn't create a request: %v\n", err)
+	}
+
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body)
+
+	if w.Code == http.StatusOK {
+		t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+	} else {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
+}
