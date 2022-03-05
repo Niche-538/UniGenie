d79492dbc34de2385900b1ebec64a95d15918cd0
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-03-04 22:47:57 -0500

    University DB Unit Testing

--- server/main_test.go
+++ server/main_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"unigenie/api/models"
+	models "unigenie/api/models"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func TestGetUniversities(t *testing.T) {
@@ -50,6 +54,47 @@ func TestGetUsers(t *testing.T) {
 
 	r.ServeHTTP(w, req)
 	fmt.Println(w.Body)
+
+	if w.Code == http.StatusOK {
+		t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+	} else {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
+}
+
+func TestAddUniversity(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	_, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	r := gin.Default()
+
+	r.POST("/addUniversity", models.PostUniversities)
+
+	university := &models.University{
+		Name:    "University of Florida",
+		Website: "www.ufl.edu",
+		Address: "4000 SW 37th Blvd",
+		City:    "Gainesville",
+		State:   "FL",
+		Zip:     32608,
+		Country: "USA",
+	}
+
+	body, _ := json.Marshal(university)
+
+	req, err := http.NewRequest(http.MethodPost, "/addUniversity", bytes.NewBuffer(body))
+
+	if err != nil {
+		t.Fatalf("Couldn't create a request: %v\n", err)
+	}
+
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
 
 	if w.Code == http.StatusOK {
 		t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
