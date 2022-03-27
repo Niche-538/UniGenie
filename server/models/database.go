30969af5615033e74a2f13cefed4f6250c2ea9ab
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-03-26 23:29:11 -0400

    Foreign Keys added

--- server/models/database.go
+++ server/models/database.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	// "gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -32,14 +31,19 @@ type Program struct {
 	InstituteName string `json:"institute_name"`
 	ProgramName   string `json:"program_name"`
 	OfferedBy     string `json:"offered_by"`
-	FallIntake    string `json:"fall_intake"`
-	SpringIntake  string `json:"spring_intake"`
+	Intake        string `json:"intake"`
+	// FallIntake    string `json:"fall_intake"`
+	// SpringIntake  string `json:"spring_intake"`
+	UniversityID uint
+	University   University `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type OfferedCourse struct {
 	gorm.Model
 	ID         uint   `gorm:"primaryKey;autoIncrement" json:"course_id"`
 	CourseName string `json:"course_name"`
+	ProgramID  uint
+	Program    Program `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 // type PersonalDetails struct {
@@ -84,52 +88,53 @@ type OfferedCourse struct {
 
 type StudentDetails struct {
 	gorm.Model
-	ID 					uint 		`gorm:"primaryKey;autoIncrement" json:"personal_details_id"`
-	FirstName 			string 		`json:"first_name"`
-	LastName  			string 		`json:"last_name"`
-	Email     			string 		`json:"email"`
-	AddressLine1 		string 		`json:"address_line_1"`
-	AddressLine2 		string 		`json:"address_line_2"`
-	City 				string 		`json:"city"`
-	State 				string 		`json:"state"`
-	ZipCode 			uint 		`json:"zip_code"`
-	Country 			string 		`json:"country"`
-	InstituteName 		string 		`json:"institute_name"`
-	InstituteCity  		string 		`json:"institute_city"`
-	InstituteState     	string 		`json:"institute_state"`
-	Degree 				string 		`json:"degree"`
-	Major			 	string 		`json:"major"`
-	CGPA 				float64		`json:"cgpa"`
-	CGPAScale 			float64		`json:"cgpa_scale"`
-	GRE	 				uint 		`json:"gre"`
-	GREVerbal			uint 		`json:"gre_verbal"`
-	GREQuant			uint 		`json:"gre_quant"`
-	GREAWM 				float64		`json:"gre_awm"`
-	TOEFL	 			uint 		`json:"toefl"`
-	TOEFLRead 			uint 		`json:"toefl_read"`
-	TOEFLListen			uint 		`json:"toefl_listen"`
-	TOEFLSpeak 			uint 		`json:"toefl_speak"`
-	TOEFLWrite 			uint 		`json:"toefl_write"`
-	IELTS	 			float64		`json:"ielts"`
-	IELTSRead 			float64		`json:"ielts_read"`
-	IELTSListen			float64		`json:"ielts_listen"`
-	IELTSSpeak 			float64		`json:"ielts_speak"`
-	IELTSWrite 			float64		`json:"ielts_write"`
+	ID             uint    `gorm:"primaryKey;autoIncrement" json:"personal_details_id"`
+	FirstName      string  `json:"first_name"`
+	LastName       string  `json:"last_name"`
+	Email          string  `json:"email"`
+	AddressLine1   string  `json:"address_line_1"`
+	AddressLine2   string  `json:"address_line_2"`
+	City           string  `json:"city"`
+	State          string  `json:"state"`
+	ZipCode        uint    `json:"zip_code"`
+	Country        string  `json:"country"`
+	InstituteName  string  `json:"institute_name"`
+	InstituteCity  string  `json:"institute_city"`
+	InstituteState string  `json:"institute_state"`
+	Degree         string  `json:"degree"`
+	Major          string  `json:"major"`
+	CGPA           float64 `json:"cgpa"`
+	CGPAScale      float64 `json:"cgpa_scale"`
+	GRE            uint    `json:"gre"`
+	GREVerbal      uint    `json:"gre_verbal"`
+	GREQuant       uint    `json:"gre_quant"`
+	GREAWM         float64 `json:"gre_awm"`
+	TOEFL          uint    `json:"toefl"`
+	TOEFLRead      uint    `json:"toefl_read"`
+	TOEFLListen    uint    `json:"toefl_listen"`
+	TOEFLSpeak     uint    `json:"toefl_speak"`
+	TOEFLWrite     uint    `json:"toefl_write"`
+	IELTS          float64 `json:"ielts"`
+	IELTSRead      float64 `json:"ielts_read"`
+	IELTSListen    float64 `json:"ielts_listen"`
+	IELTSSpeak     float64 `json:"ielts_speak"`
+	IELTSWrite     float64 `json:"ielts_write"`
+	UserID         uint
+	User           User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
+// func SetDatabase() {
+// 	db, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
+// 	if err != nil {
+// 		panic("failed to connect database")
+// 	}
 
-func SetDatabase() {
-	// db, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
-	// if err != nil {
-	// 	panic("failed to connect database")
-	// }
-
-	// Migrate the schema
-	// db.AutoMigrate(&University{})
-	// db.AutoMigrate(&User{})
-	// db.AutoMigrate(&Program{})
-	// db.AutoMigrate(&OfferedCourse{})
-	// db.AutoMigrate(&PersonalDetails{})
-	// db.AutoMigrate(&EducationDetails{})
-	// db.AutoMigrate(&StudentDetails{})
-}
+// 	// Migrate the schema
+// 	db.AutoMigrate(&University{})
+// 	db.AutoMigrate(&User{})
+// 	db.AutoMigrate(&Program{})
+// 	db.AutoMigrate(&OfferedCourse{})
+// db.AutoMigrate(&PersonalDetails{})
+// db.AutoMigrate(&EducationDetails{})
+// db.AutoMigrate(&StudentDetails{})
+// }
