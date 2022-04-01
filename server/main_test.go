1fbf5bc1b1310f639dd9d671ffd82713bf3a91a3
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-04-01 17:26:53 -0400

    Added tests for password hash functions

--- server/main_test.go
+++ server/main_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 	api "unigenie/api"
 	database "unigenie/database"
@@ -210,4 +211,59 @@ func TestAddStudentDetails(t *testing.T) {
 
 func TestDatabase(t *testing.T) {
 	assert.NoError(t, database.SetDatabaseTest())
+}
+
+func TestHashPassword(t *testing.T) {
+	user := models.User{
+		Password: "secret",
+	}
+
+	err := user.HashPassword(user.Password)
+	assert.NoError(t, err)
+
+	os.Setenv("passwordHash", user.Password)
+	fmt.Println(os.Getenv("passwordHash"))
+}
+
+// func TestCreateUserRecord(t *testing.T) {
+// 	var userResult models.User
+
+// 	err := database.SetDatabaseTest()
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	err = database.DBConn.AutoMigrate(&models.User{})
+// 	assert.NoError(t, err)
+
+// 	user := models.User{
+// 		FirstName: "Testing",
+// 		LastName:  "Usering",
+// 		Email:     "test@email.com",
+// 		Password:  os.Getenv("passwordHash"),
+// 	}
+
+// 	err = user.CreateUserRecord()
+// 	assert.NoError(t, err)
+
+// 	database.DBConn.Where("email = ?", user.Email).Find(&userResult)
+
+// 	// database.DBConn.Unscoped().Delete(&user)
+
+// 	assert.Equal(t, "Testing", userResult.FirstName)
+// 	assert.Equal(t, "Usering", userResult.LastName)
+// 	assert.Equal(t, "test@email.com", userResult.Email)
+
+// }
+
+func TestCheckPassword(t *testing.T) {
+	hash := os.Getenv("passwordHash")
+	// fmt.Println(hash)
+
+	user := models.User{
+		Password: hash,
+	}
+
+	err := user.CheckPassword("secret")
+	assert.NoError(t, err)
 }
