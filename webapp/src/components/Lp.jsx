d135b19e8e28d2b989d10504f78f3cac9f11dbea
Aakash Anil Naik <89472838+aakash-anil-naik@users.noreply.github.com>
2022-04-01 17:07:10 -0400

    More changes to color schema
    Further updates to the color schema

--- webapp/src/components/Lp.jsx
+++ webapp/src/components/Lp.jsx
@@ -1,41 +1,75 @@
 import React, { useState } from "react";
 import styles from "./landing.css";
-import {Button,Col,
+import {
+    Button,
     Container,
-    FloatingLabel,
-    Form,
     Modal,
-    Row,} from "react-bootstrap";
+    Row,
+    Form,
+    FloatingLabel,
+    Col,
+} from "react-bootstrap";
+
 const LP = () => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-   return(
-       <Container>
-    <div className="wrapper">
-    <div className="logo"> <img src="https://as2.ftcdn.net/v2/jpg/02/34/61/79/1000_F_234617921_p1AGQkGyEl8CSzwuUI74ljn6IZXqMUf2.jpg" alt=""/> </div>
-    <div className="text-center mt-4 name"> UniGenie </div>
-    <form className="p-3 mt-3">
-        <div className="form-field d-flex align-items-center"> 
-            <span className="far fa-user"></span> 
-            <input type="text" name="userName" id="userName" placeholder="Username"/> 
-        </div>
+    return (
+        <Container>
+            <div className="wrapper">
+                <div className="logo">
+                    {" "}
+                    <img
+                        src="https://as2.ftcdn.net/v2/jpg/02/34/61/79/1000_F_234617921_p1AGQkGyEl8CSzwuUI74ljn6IZXqMUf2.jpg"
+                        alt=""
+                    />{" "}
+                </div>
+                <div className="text-center mt-4 name"> UniGenie </div>
+                <form className="p-3 mt-3">
+                    <div className="form-field d-flex align-items-center">
+                        <span className="far fa-user"></span>
+                        <input
+                            type="text"
+                            name="userName"
+                            id="userName"
+                            placeholder="Username"
+                        />
+                    </div>
 
-        <div className="form-field d-flex align-items-center"> 
-            <span className="fas fa-key"></span> 
-            <input type="password" name="password" id="pwd" placeholder="Password"/> 
-        </div> 
-        <Button type="submit" variant="primary" className="btn mt-3" href="/StudentHomePage">
-           <h6>Login</h6>
-        </Button>
-        <Button type="submit" variant="primary" className="btn mt-3" href="#" onClick={handleShow}>
-        <h6>Register</h6>
-        </Button>               
-    </form>
-    <div className="text-center fs-6"> <a href="#">Forget password?</a> </div>     
-</div>
-<Modal
+                    <div className="form-field d-flex align-items-center">
+                        <span className="fas fa-key"></span>
+                        <input
+                            type="password"
+                            name="password"
+                            id="pwd"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className="btn mt-3"
+                        href="/StudentHomePage"
+                    >
+                        <h6>Login</h6>
+                    </Button>
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className="btn mt-3"
+                        href="#"
+                        onClick={handleShow}
+                    >
+                        <h6>Register</h6>
+                    </Button>
+                </form>
+                <div className="text-center fs-6">
+                    {" "}
+                    <a href="#">Forget password?</a>{" "}
+                </div>
+            </div>
+            <Modal
                 show={show}
                 onHide={handleClose}
                 backdrop="static"
@@ -56,7 +90,7 @@ const LP = () => {
                                     label="First name"
                                 >
                                     <Form.Control
-                                    style={{borderRadius:"25px"}}
+                                        style={{ borderRadius: "25px" }}
                                         type="text"
                                         placeholder="First name"
                                     />
@@ -68,7 +102,7 @@ const LP = () => {
                                     label="Last name"
                                 >
                                     <Form.Control
-                                    style={{borderRadius:"25px"}}
+                                        style={{ borderRadius: "25px" }}
                                         type="text"
                                         placeholder="Last name"
                                     />
@@ -82,7 +116,7 @@ const LP = () => {
                                 className="mb-3 mx-3"
                             >
                                 <Form.Control
-                                style={{borderRadius:"25px"}}
+                                    style={{ borderRadius: "25px" }}
                                     type="email"
                                     placeholder="name@example.com"
                                 />
@@ -93,7 +127,7 @@ const LP = () => {
                                 className="mb-3 mx-3"
                             >
                                 <Form.Control
-                                style={{borderRadius:"25px"}}
+                                    style={{ borderRadius: "25px" }}
                                     type="password"
                                     placeholder="Password"
                                 />
@@ -104,7 +138,7 @@ const LP = () => {
                                 className="mb-3 mx-3"
                             >
                                 <Form.Control
-                                style={{borderRadius:"25px"}}
+                                    style={{ borderRadius: "25px" }}
                                     type="password"
                                     placeholder="Password"
                                 />
@@ -114,15 +148,18 @@ const LP = () => {
                 </Modal.Body>
                 <Modal.Footer className="align-items-center text-center justify-content-center">
                     <Button
-                        style={{ width: "8rem",borderRadius:"25px" }}
+                        style={{ width: "8rem", borderRadius: "25px" }}
                         variant="secondary"
                         onClick={handleClose}
                     >
                         Close
                     </Button>
                     <Button
-                        style={{ width: "10rem",background:"#6C63FF", borderRadius:"25px" }}
-                        
+                        style={{
+                            width: "10rem",
+                            background: "#6C63FF",
+                            borderRadius: "25px",
+                        }}
                         type="submit"
                         variant="success"
                         href="/ProfilePage"
@@ -132,8 +169,7 @@ const LP = () => {
                     </Button>
                 </Modal.Footer>
             </Modal>
-</Container>
-   );
-
+        </Container>
+    );
 };
 export default LP;
--- webapp/src/components/ProfileSettingsPage.jsx
+++ webapp/src/components/ProfileSettingsPage.jsx
@@ -11,40 +11,40 @@ const ProfileSettingsPage = () => {
                     <Form>
                         <div className="row g-3 my-3 mx-5 justify-content-center">
                             <Form.Group
-                                className="col-xl-2"
+                                className="col-xl-2 pt-3 text-center"
                                 controlId="formFirstName"
                             >
                                 <Form.Label>First Name</Form.Label>
                             </Form.Group>
-                            <Form.Group className="col-xl-3">
+                            <Form.Group className="profile-settings-page-form-field col-xl-3 me-2">
                                 <Form.Control type="text" placeholder="John" />
                             </Form.Group>
 
                             <Form.Group
-                                className="col-xl-2"
+                                className="col-xl-2 pt-3 text-center"
                                 controlId="formLastName"
                             >
                                 <Form.Label>Last Name</Form.Label>
                             </Form.Group>
-                            <Form.Group className="col-xl-3">
+                            <Form.Group className="profile-settings-page-form-field col-xl-3 me-2">
                                 <Form.Control type="text" placeholder="Doe" />
                             </Form.Group>
                         </div>
                         <div className="row g-3 mb-3 mx-5 justify-content-center">
-                            <Form.Group className="col-xl-2">
+                            <Form.Group className="col-xl-2 pt-3 text-center">
                                 <Form.Label>Email Address</Form.Label>
                             </Form.Group>
-                            <Form.Group className="col-xl-3">
+                            <Form.Group className="profile-settings-page-form-field col-xl-3 me-2">
                                 <Form.Control
                                     type="email"
                                     placeholder="john.doe@gmail.com"
                                     readOnly
                                 />
                             </Form.Group>
-                            <Form.Group className="col-xl-2">
+                            <Form.Group className="col-xl-2 pt-3 text-center">
                                 <Form.Label>Phone Number</Form.Label>
                             </Form.Group>
-                            <Form.Group className="col-xl-3">
+                            <Form.Group className="profile-settings-page-form-field col-xl-3 me-2">
                                 <Form.Control
                                     type="number"
                                     placeholder="(123) 456-7890"
@@ -53,71 +53,81 @@ const ProfileSettingsPage = () => {
                                 />
                             </Form.Group>
                         </div>
-                        <Form.Group className="text-center justify-content-center">
-                            <Button
-                                className="update-profile-btn"
-                                type="submit"
-                            >
-                                Update Profile
-                            </Button>
-                        </Form.Group>
+                        <div className="row g-3 mb-3 mx-5 justify-content-center">
+                            <Form.Group className="col-3 text-center justify-content-center">
+                                <Button
+                                    className="update-profile-btn"
+                                    type="submit"
+                                >
+                                    Update Profile
+                                </Button>
+                            </Form.Group>
+                        </div>{" "}
                     </Form>
 
                     <div className="row my-4 mx-3 border-bottom border-1 text-center justify-content-center align-items-center"></div>
 
                     <h3 className="text-center mt-3">Change Your Password</h3>
                     <Form>
-                        <div className="row g-3 my-3 mx-5 justify-content-center">
+                        <div className="row g-3 my-1 mx-5 justify-content-center">
                             <Form.Group
-                                className="col-xl-2"
+                                className="col-xl-2 pt-3 text-center"
                                 controlId="formOldPassword"
                             >
                                 <Form.Label>Old Password</Form.Label>
                             </Form.Group>
-                            <Form.Group className="col-xl-3">
+                            <Form.Group className="profile-settings-page-form-field col-xl-3">
                                 <Form.Control
                                     type="password"
                                     placeholder="Old Password"
                                 />
                             </Form.Group>
                         </div>
-                        <div className="row g-3 mb-3 mx-5 justify-content-center">
+                        <div className="row g-3 mb-1 mx-5 justify-content-center">
                             <Form.Group
-                                className="col-xl-2"
+                                className="col-xl-2 pt-3 text-center"
                                 controlId="formNewPassword"
                             >
                                 <Form.Label>New Password</Form.Label>
                             </Form.Group>
-                            <Form.Group className="col-xl-3">
+                            <Form.Group className="profile-settings-page-form-field col-xl-3">
                                 <Form.Control
                                     type="password"
                                     placeholder="New Password"
                                 />
                             </Form.Group>
                         </div>
-                        <div className="row g-3 mb-3 mx-5 justify-content-center">
+                        <div className="row g-3 mb-1 mx-5 justify-content-center">
                             <Form.Group
-                                className="col-xl-2"
+                                className="col-xl-2 pt-3 text-center"
                                 controlId="formReenterPassword"
                             >
                                 <Form.Label>Re-enter Password</Form.Label>
                             </Form.Group>
-                            <Form.Group className="col-xl-3">
+                            <Form.Group className="profile-settings-page-form-field col-xl-3">
                                 <Form.Control
                                     type="password"
                                     placeholder="Re-enter Passsword"
                                 />
                             </Form.Group>
                         </div>
-                        <Form.Group className="text-center justify-content-center">
-                            <Button
-                                className="update-password-btn"
-                                type="submit"
-                            >
-                                Update Password
-                            </Button>
-                        </Form.Group>
+                        <div className="row g-3 mb-1 mx-5 justify-content-center">
+                            <Form.Group className="col-3 text-center justify-content-center">
+                                <Button
+                                    className="update-password-btn"
+                                    type="submit"
+                                >
+                                    Update Password
+                                </Button>
+                            </Form.Group>
+                        </div>
                     </Form>
+                </div>
+                <div>
+                    <p></p>
+                </div>
+                <div>
+                    <p></p>
                 </div>
             </div>
         </Container>
