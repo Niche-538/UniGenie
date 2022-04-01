497c3d5c716fb3e86583182d969c6bf87818cef0
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-04-01 17:26:21 -0400

    Ignore

--- server/database/database.go
+++ server/database/database.go
@@ -1,35 +1,33 @@
 package database
 
 import (
-	"unigenie/models"
-
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-// var dbConn *gorm.DB
+var DBConn *gorm.DB
 
-func SetDatabase() {
-	dbConn, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
+// func SetDatabase() {
+// 	DBConn, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
+// 	if err != nil {
+// 		panic("failed to connect database")
+// 	}
 
-	// Migrate the schema
-	dbConn.AutoMigrate(&models.University{})
-	dbConn.AutoMigrate(&models.User{})
-	dbConn.AutoMigrate(&models.Program{})
-	dbConn.AutoMigrate(&models.OfferedCourse{})
-	dbConn.AutoMigrate(&models.StudentDetails{})
-}
+// 	// Migrate the schema
+// 	DBConn.AutoMigrate(&models.University{})
+// 	DBConn.AutoMigrate(&models.User{})
+// 	DBConn.AutoMigrate(&models.Program{})
+// 	DBConn.AutoMigrate(&models.OfferedCourse{})
+// 	DBConn.AutoMigrate(&models.StudentDetails{})
+// }
 
 func SetDatabaseTest() (err error) {
-	dbConn, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
+	DBConn, err := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
 	if err != nil {
 		return err
 	}
 
-	dbConn.DB()
+	DBConn.DB()
 
-	return
+	return nil
 }
