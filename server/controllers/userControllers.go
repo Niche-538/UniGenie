2730100adbf9e13405784b37e6e8f508aca16136
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-04-01 22:29:04 -0400

    Added token generator

--- server/controllers/userControllers.go
+++ server/controllers/userControllers.go
@@ -1,44 +1,118 @@
 package controllers
 
-// "fmt"
-// "time"
-// "log"
-// "strconv"
-// "context"
-// helper "unigenie/helpers"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"unigenie/auth"
+	"unigenie/database"
+	models "unigenie/models"
 
-// "unigenie/helpers"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
-// var validate = validator.New()
+// Signup creates a user in db
+func Signup(c *gin.Context) {
+	var newUser models.User
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-// // pointer receivers
+	db, sht := gorm.Open(sqlite.Open("unigenie.db"), &gorm.Config{})
+	if sht != nil {
+		panic("failed to connect database")
+	}
 
-// func (user *models.User) PasswordHash(password string) error {
-// 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-// 	if err != nil {
-// 		return err
-// 	}
+	fmt.Println("Password Signup: ", newUser.Password, "\t\t Type: %T", newUser.Password)
 
-// 	user.Password = string(bytes)
-// 	return nil
-// }
+	err, pwd := newUser.HashPassword(newUser.Password)
+	if err != nil {
+		panic("failed to create a hash")
+	} else {
+		fmt.Println("Hash Password: ", pwd)
+	}
 
-// func VerifyPassword() {
+	user := models.User{FirstName: newUser.FirstName, LastName: newUser.LastName, Email: newUser.Email, Password: pwd}
+	db.Create(&user)
 
-// }
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
 
-// func Login() {
+type LoginPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
-// }
+// LoginResponse token response
+type LoginResponse struct {
+	Token string `json:"token"`
+}
 
-// func SignUp() {
+func Login(c *gin.Context) {
 
-// }
+	var payload LoginPayload
+	var user models.User
 
-// func GetUsers() {
+	err := c.ShouldBindJSON(&payload)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"msg": "invalid json",
+		})
+		c.Abort()
+		return
+	}
 
-// }
+	fmt.Print("#########\n\n")
+	fmt.Println(payload.Email, "\t\t", payload.Password)
+	fmt.Print("\n\n #########\n")
 
-// func GetUser() {
+	result := database.ReturnDatabase().Where("email = ?", payload.Email).First(&user)
 
-// }
+	fmt.Print("#########\n\n")
+	fmt.Println(result)
+	fmt.Print("\n\n #########\n")
+
+	if result.Error == gorm.ErrRecordNotFound {
+		c.JSON(401, gin.H{
+			"msg": "invalid user credentials",
+		})
+		c.Abort()
+		return
+	}
+
+	err = user.CheckPassword(payload.Password)
+	if err != nil {
+		log.Println(err)
+		c.JSON(401, gin.H{
+			"msg": "invalid user credentials",
+		})
+		c.Abort()
+		return
+	}
+
+	jwtWrapper := auth.JwtWrapper{
+		SecretKey:       "verysecretkey",
+		Issuer:          "AuthService",
+		ExpirationHours: 24,
+	}
+
+	signedToken, err := jwtWrapper.GenerateToken(user.Email)
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"msg": "error signing token",
+		})
+		c.Abort()
+		return
+	}
+
+	tokenResponse := LoginResponse{
+		Token: signedToken,
+	}
+
+	c.JSON(200, tokenResponse)
+
+}
