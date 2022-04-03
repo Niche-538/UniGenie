07af652af846d93aee1aae75d076cb6c99711ecd
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-04-03 09:48:48 -0400

    Commented out

--- server/routes/authRouter.go
+++ server/routes/authRouter.go
@@ -1,12 +1,12 @@
 package routes
 
-import (
-	controller "unigenie/controllers"
+// import (
+// 	controller "unigenie/controllers"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-func AuthRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("/user/signup", controller.SignUp())
-	incomingRoutes.POST("/user/login", controller.Login())
-}
+// func AuthRoutes(incomingRoutes *gin.Engine) {
+// 	incomingRoutes.POST("/user/signup", controller.SignUp())
+// 	incomingRoutes.POST("/user/login", controller.Login())
+// }
--- server/routes/userRouter.go
+++ server/routes/userRouter.go
@@ -1,19 +1,19 @@
 package routes
 
-import (
-	controller "unigenie/controllers"
-	middleware "unigenie/middleware"
+// import (
+// 	controller "unigenie/controllers"
+// 	middleware "unigenie/middleware"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-func UserRoutes(incomingRoutes *gin.Engine) {
-	// routes here require middleware as they are protected routes.
-	// This means that the users must have a token for accessing these routes.
-	// User must not be able to use protected routes without logging in the system (i.e, token is required).
-	incomingRoutes.Use(middleware.Authenticate())
-	incomingRoutes.GET("/users", controller.GetUsers())
-	// incomingRoutes.GET("/users/:user_id", controller.GetUser())
-	incomingRoutes.GET("/universities", controller.GetUniversities())
-	// incomingRoutes.GET("/universities/:university_id", controller.GetUniversity())
-}
+// func UserRoutes(incomingRoutes *gin.Engine) {
+// 	// routes here require middleware as they are protected routes.
+// 	// This means that the users must have a token for accessing these routes.
+// 	// User must not be able to use protected routes without logging in the system (i.e, token is required).
+// 	incomingRoutes.Use(middleware.Authenticate())
+// 	incomingRoutes.GET("/users", controller.GetUsers())
+// 	// incomingRoutes.GET("/users/:user_id", controller.GetUser())
+// 	incomingRoutes.GET("/universities", controller.GetUniversities())
+// 	// incomingRoutes.GET("/universities/:university_id", controller.GetUniversity())
+// }
