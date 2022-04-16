f526fcfcb5ad5b89826751b97a499d26ae8608d9
Kunal D <kunaldudhe@gmail.com>
2022-04-16 15:50:50 -0400

    Created resources pages
    Created resources pages, dropdown menu and code cleanup

---
+++ webapp/src/components/Mech.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Mech = () => {
+  return (
+    <div>Mech</div>
+  )
+}
+
+export default Mech;
---
+++ webapp/src/components/UK.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UK = () => {
+  return (
+    <div>UK</div>
+  )
+}
+
+export default UK;
