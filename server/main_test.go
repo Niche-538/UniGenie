305c3a975d84d8bb68e192dcf2401c19710d69b5
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-04-21 02:35:47 -0400

    Testing - GET API for Blogs

--- server/main_test.go
+++ server/main_test.go
@@ -499,3 +499,27 @@ func TestGenerateToken(t *testing.T) {
 
 	os.Setenv("TestToken", testToken)
 }
+
+func TestGetBlogs(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	r := gin.Default()
+	r.GET("/blogs", api.GetBlogs)
+
+	req, err := http.NewRequest(http.MethodGet, "/blogs", nil)
+
+	if err != nil {
+		t.Fatalf("Couldn't create a request: %v\n", err)
+	}
+
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body)
+
+	if w.Code == http.StatusOK {
+		t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+	} else {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
+}
